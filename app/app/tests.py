f9b6f359d45e3b4551fc898e3d2b664635d46ec7
Keletsomaleka <85512610+Keletsomaleka@users.noreply.github.com>
2023-10-04 15:43:45 +0200

    Lint fix and db set up on settings

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,17 +7,19 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
+        
         """test addition of numbers"""
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
 
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
